ba4cc56f81324167cc7007d8fd7fa85a34b1ac5c
NickOsipov <nick.osipov.91@gmail.com>
2024-01-13 16:15:21 +0300

    feat: deduplication

--- scripts/send_news.py
+++ scripts/send_news.py
@@ -1,5 +1,6 @@
 import os
 from time import sleep
+import logging
 
 from telebot import TeleBot, formatting
 import requests
@@ -10,17 +11,28 @@ from news_sources.habr_news_ai_source import HabrNewsAISource
 token = os.getenv("BOT_TOKEN")
 bot = TeleBot(token=token, parse_mode="html", disable_web_page_preview=True)
 chat_id = os.getenv("CHAT_ID")
+news_to_post = int(os.environ.get("NEWS_TO_POST") or 3)
 source = HabrNewsAISource()
 
 
 if __name__ == "__main__":
-    news_to_post = source.get_news()
+    logging.info(f"News to post: {news_to_post}")
 
-    for news in news_to_post:
+    while news_to_post:
+        news = source.get_one_news()
+        if not news:
+            continue
+
         photo = source.create_mem_from_photo(news=news)
         caption = source.construct_caption(news=news)
 
-        bot.send_photo(chat_id=chat_id, photo=open(photo, "rb"), caption=caption)
+        bot.send_photo(
+            chat_id=chat_id,
+            photo=open(photo, 'rb'),
+            caption=caption
+        )
+        logging.info(f"News {news.title} was sent")
         photo.unlink()
-        print(f"News {caption} was send")
         sleep(5)
+        news_to_post -= 1
+
